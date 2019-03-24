6fcd65fb8d796e63017aefab96386d20abf84569
Yuniar Pristyan Chandra <y.pristyan.chandra@gmail.com>
2019-03-24 20:46:15 +0700

    build release

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_movie/ui/main/main.dart';
+import 'package:flutter_movie/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
